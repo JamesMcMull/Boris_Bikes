e66eab2bf79b7925d9f136178055b196393a9ef6
Charlie Slater <charliemslater1701@gmail.com>
2021-04-29 15:47:46 +0100

    added private full? and empty? methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,19 @@ class DockingStation
         @bikes
     end
     def release_bike
-        fail 'No bikes' if @bikes.empty?
-        @bikes.pop
+      fail 'No bikes' if empty?
+      @bikes.pop
     end
     def docking_bike(bike)
-        fail 'No space in dock' if @bikes.length >= 20
-        @bikes << bike
+      fail 'No space in dock' if full?
+      @bikes << bike
+    end
+    private
+    
+    def full?
+      @bikes.length >= 20
+    end
+    def empty?
+      @bikes.empty?
     end
 end
-
