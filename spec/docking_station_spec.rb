4567ebffd2ad42a83961bab83d77690f2407cac3
root <mcmullan.jgh@gmail.com>
2021-04-30 17:31:55 +0100

    isolated tests with double

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,9 +15,10 @@ describe DockingStation do
 
     describe '#release_bike' do
         it 'releases a working bike' do
-            bike = Bike.new
+          bike = double(:bike)
+          allow(bike).to receive(:broken).and_return(false)
             subject.docking_bike(bike)
-            expect(bike).to respond_to(:working?)
+            #expect(bike).to respond_to(:working?)
             expect(subject.release_bike).to eq bike
         end
     end
@@ -27,18 +28,21 @@ describe DockingStation do
         expect { subject.release_bike }.to raise_error 'No bikes'
       end
       it 'accepts all bikes' do
-        bike = Bike.new
-        bike.is_broken
+        bike = double(:bike)
+        #bike.is_broken
+        allow(bike).to receive(:broken).and_return(false)
         subject.docking_bike(bike)
-        bike_two = Bike.new
+        bike_two = double(:bike)
         subject.docking_bike(bike_two)
         expect(subject.bikes.length).to eq(2)
       end
         it 'only releases working bikes' do
-        bike = Bike.new
-        bike.is_broken
+        bike = double(:bike)
+        #bike.is_broken
+        allow(bike).to receive(:broken).and_return(true)
         subject.docking_bike(bike)
-        bike_two = Bike.new
+        bike_two = double(:bike)
+        allow(bike_two).to receive(:broken).and_return(false)
         subject.docking_bike(bike_two)
         2.times { subject.release_bike }
         expect(subject.bikes.length).to eq(1)
@@ -48,19 +52,21 @@ describe DockingStation do
   describe '#docking_bike' do
     it { is_expected.to respond_to(:docking_bike).with(1).argument }
     it 'checks last bike is last bike when docked' do
-      bike = Bike.new
+      bike = double(:bike)
       subject.docking_bike(bike)
       bikes = subject.bikes
       expect(bikes[-1]).to eq bike
     end
     it 'docks a bike with 2 parameters' do
-      bike = Bike.new
-      subject.docking_bike(bike, true)
+      bike = double(:bike)
+      allow(bike).to receive(:broken).and_return(true)
+      subject.docking_bike(bike)
       expect(bike.broken).to eq(true)
     end
     it 'raises an error if no dock' do
-      DockingStation.new.capacity.times {subject.docking_bike(Bike.new)}
-      expect { subject.docking_bike Bike.new }.to raise_error 'No space in dock'
+      bike = double(:bike)
+      DockingStation.new.capacity.times {subject.docking_bike(bike)}
+      expect { subject.docking_bike (bike) }.to raise_error 'No space in dock'
     end
   end
 end
